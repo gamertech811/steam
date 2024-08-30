7a8c5e964af3f1b8d3fcd44b7da1f8a8d608057a
gamertech811 <borabrawlstars@gmail.com>
2024-08-30 17:35:47 -0300

    terminei a barrinha lateral 30/08

---
+++ src/app/components/styles/navbar.js
@@ -0,0 +1,44 @@
+import "./navbar.css"
+
+export default function NavBar() {
+    return (
+        <gridnav>
+            <span>
+                <img src="/steam.png" id="steamlogo"></img>
+                <a href="/" id="acoisado">‎ </a>
+            </span>
+            <nav id="flexcaixa">
+                
+
+                <loja>
+                    <a href="/">LOJA</a>
+                    <span id="hoverzinho">
+                        <p className="trecoco">inicio</p>
+                        <p className="trecoco">lista de slaoq</p>
+                        <p className="trecoco">coisa3</p>
+                        <p className="trecoco">kamehameha</p>
+                    </span>
+                </loja>
+
+                <comu>
+                    <a href="/">COMUNIDADE</a>
+                    <span id="hoverzinho">
+                        <p className="trecoco">mercado noturno</p>
+                        <p className="trecoco">lei baba</p>
+                        <p className="trecoco">sarrada</p>
+                        <p className="trecoco">kjesuita</p>
+                    </span>
+                </comu>
+
+                <sobre>
+                    <a href="/">SOBRE</a>
+                </sobre>
+
+                <supor>
+                    <a href="/">SUPORTE</a>
+                </supor>
+
+            </nav>
+        </gridnav>
+    );
+}
---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+      <div>
+        <div id="align">
+          <p>Sua Loja</p>
+          <p>Novidades</p>
+          <p>Categorias</p>
+          <p>Loja de Pontos</p>
+          <p>Notícias</p>
+          <p>Laboratório</p>
+          <espaco></espaco>
+          <span id="spancado">
+            <input type="text" id="pesquisar" placeholder="procurar"></input>
+            <img src="/semmouse.png" id="pesq"></img>
+          </span>
+        </div>
+      </div>
+  );
+}
