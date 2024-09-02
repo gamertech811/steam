acae01eafffce2e99a82c28f389fa784bde6f036
gamertech811 <borabrawlstars@gmail.com>
2024-09-01 23:34:45 -0300

    finalizado 01/09

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,32 @@ export default function Home() {
   return (
     <div>
       <div id="align">
-        <p>Sua Loja</p>
-        <p>Novidades</p>
-        <p>Categorias</p>
-        <p>Loja de Pontos</p>
-        <p>Notícias</p>
-        <p>Laboratório</p>
+        <span>
+          <p id="rapido">Sua Loja</p>
+          <lojahover>
+            <a href="/">Pagina inicial</a>
+            <a href="/">Recomendaçoes</a>
+            <a href="/">Recentes</a>
+            <a href="/" id="pad">
+              Curadores
+            </a>
+          </lojahover>
+        </span>
+        <span>
+          <p>Novidades</p>
+        </span>
+        <span>
+          <p>Categorias</p>
+        </span>
+        <span>
+          <p>Loja de Pontos</p>
+        </span>
+        <span>
+          <p>Notícias</p>
+        </span>
+        <span>
+          <p>Laboratório</p>
+        </span>
         <espaco></espaco>
         <span id="spancado">
           <input type="text" id="pesquisar" placeholder="procurar"></input>
