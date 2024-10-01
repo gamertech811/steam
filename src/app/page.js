c25a85add27b4afe9cd52497389bbd496168a4bd
gamertech811 <borabrawlstars@gmail.com>
2024-10-01 16:58:24 -0300

    finished

--- src/app/page.js
+++ src/app/page.js
@@ -16,10 +16,24 @@ export default function Home() {
           </lojahover>
         </span>
         <span>
-          <p>Novidades</p>
+            <p id="rapido">Novidades</p>
+            <lojahover>
+              <a href="/">Lancamentos</a>
+              <a href="/">Populares</a>
+              <a href="/">Para Voce</a>
+              <a href="/" id="pad">Escolhas da Comunidade</a>
+            </lojahover>
         </span>
         <span>
-          <p>Categorias</p>
+            <p id="rapido">Jogos</p>
+            <lojahover>
+              <a href="/">Valorant</a>
+              <a href="/">CS2</a>
+              <a href="/">Brawhalla</a>
+              <a href="/">Hollow Knight</a>
+              <a href="/">Brawl Stars</a>
+              <a href="/" id="pad">Free Fire</a>
+            </lojahover>
         </span>
         <span>
           <p>Loja de Pontos</p>
@@ -36,7 +50,22 @@ export default function Home() {
           <img src="/semmouse.png" id="pesq"></img>
         </span>        
       </div>
-      
+      <div id="teste">
+      <div className="www" id="ww">
+      <img src="/cs.jpg"></img>
+          <div id="black">
+            <p>CS2</p>
+            <h3>oi ze do grau 123 é nois coroa fera uhullll beleza arislsls ald waa what your name uau sem vergonha lele da cuca.<br></br><br></br>Baixe ja    o:</h3>
+          </div>  
+      </div>
+        <caixinhas>
+          <div className="categorias"></div>
+          <div className="categorias"></div>
+          <div className="categorias"></div>
+          <div className="categorias"></div>
+        </caixinhas>
+        
+      </div>
     </div>
   );
 }
